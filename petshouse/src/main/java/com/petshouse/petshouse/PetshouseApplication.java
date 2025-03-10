c73a067eb0c34d14a15e5eec93c63cae3872331f
artmkchmv <artemkakrut163@gmail.com>
2025-03-10 11:42:41 +0400

    Create a directory

---
+++ petshouse/src/main/java/com/petshouse/petshouse/PetshouseApplication.java
@@ -0,0 +1,13 @@
+package com.petshouse.petshouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshouseApplication.class, args);
+	}
+
+}
---
+++ petshouse/src/test/java/com/petshouse/petshouse/PetshouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshouse.petshouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
