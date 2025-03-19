50b31f8aaee8bfe8b7c554a8a281a26085a197da
artmkchmv <artemkakrut163@gmail.com>
2025-03-19 21:13:17 +0400

    Pet.java refactor

--- petshouse/src/main/java/com/petshouse/petshouse/entity/Pet.java
+++ petshouse/src/main/java/com/petshouse/petshouse/entity/Pet.java
@@ -12,27 +12,28 @@ import lombok.*;
 public class Pet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pet_id")
     private Long petId;
 
-    @Column(nullable = false, length = 50)
+    @Column(name = "pet_name", nullable = false, length = 50)
     private String petName;
 
-    @Column(nullable = false)
+    @Column(name = "pet_age", nullable = false)
     private int petAge;
 
-    @Column(nullable = false, length = 50)
+    @Column(name = "pet_type", nullable = false, length = 50)
     private String petType;
 
-    @Column(nullable = false, columnDefinition = "TEXT")
+    @Column(name = "pet_description", nullable = false, columnDefinition = "TEXT")
     private String petDescription;
 
-    @Column(nullable = false, length = 20)
+    @Column(name = "pet_status", nullable = false, length = 20)
     private String petStatus = "available";
 
     @ManyToOne
     @JoinColumn(name = "petOwnerId", nullable = false)
     private User petOwner;
 
-    @Column(nullable = false, columnDefinition = "TEXT")
+    @Column(name = "pet_photo_url", nullable = false, columnDefinition = "TEXT")
     private String petPhotoURL;
 }
