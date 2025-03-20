366ac0ed004f3c4dc339ddf384b52e3f3ba1849d
artmkchmv <artemkakrut163@gmail.com>
2025-03-20 23:37:19 +0400

    Refactor FavoriteRepository.java

--- petshouse/src/main/java/com/petshouse/petshouse/repository/FavoriteRepository.java
+++ petshouse/src/main/java/com/petshouse/petshouse/repository/FavoriteRepository.java
@@ -3,8 +3,8 @@ package com.petshouse.petshouse.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.petshouse.petshouse.entity.Favorite;
+import com.petshouse.petshouse.entity.*;
 
 @Repository
-public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> {
 }
