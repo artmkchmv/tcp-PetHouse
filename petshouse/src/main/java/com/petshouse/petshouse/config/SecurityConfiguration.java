c01fe9d6869ff6aa2fd33df9b9e201f899c83ff3
artmkchmv <artemkakrut163@gmail.com>
2025-04-18 13:17:55 +0400

    Update SecurityConfiguration.java

--- petshouse/src/main/java/com/petshouse/petshouse/config/SecurityConfiguration.java
+++ petshouse/src/main/java/com/petshouse/petshouse/config/SecurityConfiguration.java
@@ -12,8 +12,7 @@ public class SecurityConfiguration {
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(authz -> authz
                 .anyRequest().permitAll()
             );
