e9934cdda19d8716d44270906bf66a91136fc073
artmkchmv <artemkakrut163@gmail.com>
2025-05-29 23:38:47 +0400

    Update Repositories

--- petshouse/src/main/java/com/petshouse/petshouse/repository/FavoriteRepository.java
+++ petshouse/src/main/java/com/petshouse/petshouse/repository/FavoriteRepository.java
@@ -1,8 +1,11 @@
 package com.petshouse.petshouse.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.petshouse.petshouse.entity.*;
@@ -11,4 +14,7 @@ import com.petshouse.petshouse.entity.*;
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
 
     List<Favorite> findByUserId(Long userId);
+    
+    @Query("SELECT f FROM Favorite f WHERE f.user.id = :userId AND f.pet.id = :petId")
+    Optional<Favorite> findByUserIdAndPetId(@Param("userId") Long userId, @Param("petId") Long petId);
 }
--- petshouse/src/main/java/com/petshouse/petshouse/repository/MessageRepository.java
+++ petshouse/src/main/java/com/petshouse/petshouse/repository/MessageRepository.java
@@ -17,4 +17,27 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
            "(m.sender.id = :userId2 AND m.receiver.id = :userId1) " +
            "ORDER BY m.messageTimeStamp ASC")
     List<Message> findConversationBetweenUsers(@Param("userId1") Long userId1, @Param("userId2") Long userId2);
+
+    @Query("SELECT m FROM Message m WHERE m.sender.id = :userId OR m.receiver.id = :userId ORDER BY m.messageTimeStamp ASC")
+    List<Message> findAllByUserId(@Param("userId") Long userId);
+
+    @Query("""
+        SELECT 
+            CASE WHEN m.sender.id = :userId THEN m.receiver.id ELSE m.sender.id END AS otherUserId,
+            CASE WHEN m.sender.id = :userId THEN m.receiver.login ELSE m.sender.login END AS otherUserLogin,
+            m.messageText AS lastMessageText,
+            m.messageTimeStamp AS lastMessageTimestamp
+        FROM Message m
+        WHERE m.sender.id = :userId OR m.receiver.id = :userId
+        AND m.messageTimeStamp = (
+            SELECT MAX(m2.messageTimeStamp)
+            FROM Message m2
+            WHERE 
+                (m2.sender.id = m.sender.id AND m2.receiver.id = m.receiver.id)
+                OR
+                (m2.sender.id = m.receiver.id AND m2.receiver.id = m.sender.id)
+        )
+        ORDER BY m.messageTimeStamp DESC
+        """)
+    List<Object[]> findDialogsByUserId(@Param("userId") Long userId);
 }
